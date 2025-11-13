08bc9d53e415c7820c5c5b20ecb09be0c9d71e09
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-11-12 21:18:30 -0500

    refactor(assets): Centralizar todos los JS y CSS en Webpack Encore

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,15 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css';
 
-console.log('TKOH Bookstore cargado!');
+import './js/main.js';
+import './js/navbar.js';
+
+import './js/pages/home.js';
+import './js/pages/detail.js';
+import './js/pages/search.js';
+
+import './js/modules/api.js';
+import './js/modules/cart.js';
+import './js/modules/checkout.js';
+import './js/modules/storage.js';
+import './js/modules/ui.js';
